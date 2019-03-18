f2d174ccb3b00ded10e6b687db6a3bd8b19efd81
Renee Nordholm <reneenordholm@gmail.com>
2019-03-18 11:45:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,8 +11,7 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-def speak_to_grandma
-  require_relative = "I LOVE YOU GRANDMA!"
+def speak_to_grandma(require_relative = "I LOVE YOU GRANDMA!")
   if require_relative == "Hi Nana, how are you?"
     puts "HUH?! SPEAK UP, SONNY!"
   elsif require_relative == "WHAT DID YOU EAT TODAY?"
