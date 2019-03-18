34e394a9b1f49b3a1ab91569f2383582b18f0741
Renee Nordholm <reneenordholm@gmail.com>
2019-03-18 11:47:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,8 @@
 def speak_to_grandma(require_relative = "I LOVE YOU GRANDMA!")
   if require_relative == "Hi Nana, how are you?"
     puts "HUH?! SPEAK UP, SONNY!"
+  elsif require_relative == "Hi!"
+    puts "HUH?! SPEAK UP, SONNY!"
   elsif require_relative == "WHAT DID YOU EAT TODAY?"
     puts "NO, NOT SINCE 1938!"
   else "I LOVE YOU TOO PUMPKIN!"
